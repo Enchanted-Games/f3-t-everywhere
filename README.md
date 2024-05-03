9d8abe6b92374912d0fa8515858e3c5cd13b2151
ioblackshaw <58823778+Enchanted-Games@users.noreply.github.com>
2024-05-03 19:18:10 +0100

    backport to 1.14

--- README.md
+++
@@ -1,2 +0,0 @@
-# f3-t-everywhere
-A fabric mod that lets you reload resourcepack with F3+T while in menus
--- src/main/java/games/enchanted/f3teverywhere/mixin/KeyboardMixin.java
+++ src/main/java/games/enchanted/f3teverywhere/mixin/KeyboardMixin.java
@@ -3,7 +3,6 @@ package games.enchanted.f3teverywhere.mixin;
 import net.minecraft.client.Keyboard;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.util.Util;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -16,30 +15,20 @@ public abstract class KeyboardMixin {
 	@Shadow
 	MinecraftClient client;
 	boolean switchF3State;
-	long debugCrashStartTime = -1L;
 
 	@Shadow
-	abstract public void debugLog(String key, Object... args);
-
-	private boolean reloadResources(int key) {
-		if (this.debugCrashStartTime > 0L && this.debugCrashStartTime < Util.getMeasuringTimeMs() - 100L) {
-			return true;
-		} else if (key == 84) {
-			this.debugLog("debug.reload_resourcepacks.message");
-			this.client.reloadResources();
-			return true;
-		} else {
-			return false;
-		}
-	};
+	abstract public boolean processF3(int key);
 
 	@Inject(at = @At("HEAD"), method = "Lnet/minecraft/client/Keyboard;onKey(JIIII)V")
 	public void onKey(long window, int key, int scancode, int action, int modifiers, CallbackInfo ci) {
-		if (window == this.client.getWindow().getHandle()) {
+		if (window == this.client.window.getHandle()) {
 			boolean bl2;
 			if (action != 0 && this.client.currentScreen != null) {
-				bl2 = InputUtil.isKeyPressed(MinecraftClient.getInstance().getWindow().getHandle(), 292)
-						&& this.reloadResources(key);
+				try {
+					bl2 = InputUtil.isKeyPressed(this.client.window.getHandle(), 292) && this.processF3(key);
+				} catch (Exception e) {
+					bl2 = true;
+				}
 				this.switchF3State |= bl2;
 			}
 		}
